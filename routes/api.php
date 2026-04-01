d9b77a45ea33b75b34f4bf70cdf8811a6e64cb7e
Juan Camilo Marin Zapata <marin.jc2005@gmail.com>
2026-04-01 14:38:34 -0500

    Por ultimo cree las rutas y con esto concluye el modulo de lugares

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\LugaresController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/lugares', [LugaresController::class, 'index']);
+Route::get('/lugares/{id}', [LugaresController::class, 'show']);
