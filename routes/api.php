6c0974f864b8129e98bb9c3cdbed4232ad1bb5f5
Juan Camilo Marin Zapata <marin.jc2005@gmail.com>
2026-04-01 15:25:49 -0500

    Por ultimo cree las rutas y con esto concluye el modulo de hospedajes

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\LugaresController;
+use App\Http\Controllers\HospedajeController;
+
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/lugares', [LugaresController::class, 'index']);
 Route::get('/lugares/{id}', [LugaresController::class, 'show']);
+Route::get('/hospedajes', [HospedajeController::class, 'index']);
+Route::get('/hospedajes/{id}', [HospedajeController::class, 'show']);
