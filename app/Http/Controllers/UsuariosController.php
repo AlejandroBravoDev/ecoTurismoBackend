d3dc4ddad8a6744351834a758ddea77cf0ea4089
AlejandroBravoDev <alebetan09@gmail.com>
2026-04-04 11:55:25 -0500

    estructura del controlador

---
+++ app/Http/Controllers/UsuariosController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usuario;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class UsuariosController extends Controller
+{
+    
+}
